7e7ca18504acc3c59bae4a92c2411956823a9cfb
otofune <otofune@gmail.com>
2016-06-09 21:56:11 +0900

    update

--- sample/config.sh
+++ sample/config.sh
@@ -1,4 +1,6 @@
 # twitter.com/siguroidで利用しているスクリプトを置くディレクトリに配置するconfig.sh
+startinit="$source の repo init を行います。\n$startinittime #sigurebuild"
+stopinit="$source の repo init が異常終了しました。\n$stopinittime #sigurebuild"
 startsync="$source の repo sync を開始します。\n$startsynctime #sigurebuild"
 stopsync="$source の repo sync が異常終了しました。\n$endsynctime #sigurebuild"
 endsync="$source の repo sync が正常終了しました。\n$endsynctime #sigurebuild"
