9a576be035c532786c5f47663df1c37e7c4e266b
otofune <otofune@gmail.com>
2016-06-11 00:56:07 +0900

    sample: fix

--- sample/config.sh
+++ sample/config.sh
@@ -5,6 +5,12 @@ startsync="$source の repo sync を開始します。\n$startsynctime #sigurebu
 stopsync="$source の repo sync が異常終了しました。\n$endsynctime #sigurebuild"
 endsync="$source の repo sync が正常終了しました。\n$endsynctime #sigurebuild"
 starttwit="$model 向け $source のビルドを開始します。\n$starttime #sigurebuild"
-stoptwit="$model 向け $source のビルドが失敗しました。\n$endstr\n$endtime #sigurebuild"
-endtwit="$model 向け $source のビルドが成功しました!\n$endstr\n$endtime #sigurebuild"
-endziptwit="$zipname のビルドに成功しました!\n$endstr\n$endtime #sigurebuild"
+if [ "$endstr" = "" ]; then
+	stoptwit="$model 向け $source のビルドが失敗しました。\n$endtime #sigurebuild"
+	endtwit="$model 向け $source のビルドが成功しました!\n$endtime #sigurebuild"
+	endziptwit="$zipname のビルドに成功しました!\n$endtime #sigurebuild"
+else
+	stoptwit="$model 向け $source のビルドが失敗しました。\n$endstr\n$endtime #sigurebuild"
+	endtwit="$model 向け $source のビルドが成功しました!\n$endstr\n$endtime #sigurebuild"
+	endziptwit="$zipname のビルドに成功しました!\n$endstr\n$endtime #sigurebuild"
+fi
