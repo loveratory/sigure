b38f109d556311d8635e316c639a2cb23ad74eca
otofune <otofune@gmail.com>
2016-07-01 13:43:44 +0000

    add repo init option and fix

--- build.sh
+++ build.sh
@@ -8,19 +8,25 @@ unset zip_folder_name
 unset arg_name
 unset source_dir
 unset target_device
+unset repo_init_uri
 unset make_clean
 unset repo_sync
 unset tweet
 unset make_mode
-unset screen
+unset screen_skip
 unset finish
 unset source_name
 unset model_name
 unset temp
 unset get_build_var_cm
+unset start_init_time
+unset start_init_tweet
+unset stop_init_time
+unset stop_init_tweet
 unset start_sync_time
 unset start_sync_tweet
 unset res_sync
+unset res_breakfast
 unset end_sync_time
 unset end_sync_tweet
 unset stop_sync_tweet
@@ -42,10 +48,8 @@ pararell_jobs=4
 log_folder_name="logs"
 zip_folder_name="zips"
 
-# 基幹設定ファイル読み込み
-
-if [ -f ../config.sh ]; then
-    . ../config.sh
+if [ -f ./config.sh ]; then
+    . ./config.sh
 fi
 
 # フォルダ作成
@@ -67,77 +71,82 @@ function color () {
     echo -e "\033[$1m$2\033[m" $3
 }
 
-function usage_exit () {
-    echo -e "\n使用法: $0 [-d dir] [-r "device"] [-c] [-j thread] [-m] [-s] [-t] [-x]" 1>&2
-    echo -e "-d: ソースディレクトリを現在位置からの相対パスか絶対パス" 1>&2
-    echo -e "-r: 実行するデバイスネーム" 1>&2
-    echo -e "-c: make cleanを行う(オプション)" 1>&2
-    echo -e "-j: ジョブ数(オプション)" 1>&2
-    echo -e "-m: brunchではなくmakeで行う(オプション)" 1>&2
-    echo -e "-s: repo syncを行う(オプション)" 1>&2
-    echo -e "-t: ツイートを行う(オプション)" 1>&2
-    echo -e "-x: チェック機構を全てスキップ(オプション)" 1>&2
-    echo -e "Enterを押すまで終了を待機します..." 1>&2
-    read temp
-    exit 1
+function usage () {
+    echo -e "使用法: $0 [-d "dir"] [-r "device"] [-i "URI"] [-c] [-j thread] [-m] [-s] [-t] [-x] [-h]" $1
+    echo -e "-d: ソースディレクトリの位置" $1
+    echo -e "-r: 実行するデバイスネーム" $1
+    echo -e "-i: repo initを行う(オプション)" $1
+    echo -e "-c: make cleanを行う(オプション)" $1
+    echo -e "-j: ジョブ数(オプション)" $1
+    echo -e "-m: brunchではなくmakeで行う(オプション)" $1
+    echo -e "-s: repo syncを行う(オプション)" $1
+    echo -e "-t: ツイートを行う(オプション)" $1
+    echo -e "-x: screenを使用しない(オプション)" $1
+    echo -e "-h: ヘルプ表示(オプション)" $1
 }
 
-function error () {
-    echo -e "\n動作環境がセットアップされていないようです。" 1>&2
-    echo -e "以下のREADMEを十分に読みセットアップを行ってください。" 1>&2
-    echo -e "URL: https://github.com/otofune/sigure/blob/master/readme.md" 1>&2
-    echo -e "Enterを押すまで終了を待機します..." 1>&2
-    read temp
-    exit 1
-}
+# 引数取得
 
-# 引数処理
-
-while getopts d:r:j:cstmx arg_name
+while getopts d:r:i:j:cstmxh arg_name
 do
     case $arg_name in
         d) source_dir=$OPTARG ;;
         r) target_device=$OPTARG ;;
+        i) repo_init_uri=$OPTARG ;;
         c) make_clean="true" ;;
         s) repo_sync="true" ;;
         t) tweet="true" ;;
         j) pararell_jobs=$OPTARG ;;
         m) make_mode="true" ;;
-        x) skip="true" ;;
+        x) screen_skip="true" ;;
+        h) usage
+           exit 0 ;;
     esac
 done
 
 # エラー処理
 
-if [ "$skip" != "true" ]; then
+if [ "$screen_skip" != "true" ]; then
 
     # screenの存在をチェック
 
     (type screen) >& /dev/null
     if [ $? -ne 0 ]; then
         color $red "screenがシステムにインストールされていません。" 1>&2
-        error
+        echo -e "screenを利用しない場合は-xオプションを使用してください。" 1>&2
+        exit 1
     fi
+    
+fi
 
-    # ソースディレクトリ、デバイスツリーディレクトリの存在をチェック
+# ソースディレクトリの存在をチェック
 
-    if [ "$source_dir" = "" ]; then
-        color $red "ディレクトリが指定されていません。" 1>&2
-        finish="true"
-    elif [ ! -d $source_dir ]; then
+if [ "$source_dir" = "" ]; then
+    color $red "ディレクトリが指定されていません。" 1>&2
+    finish="true"
+elif [ ! -d $source_dir ]; then
+    if [ "$repo_init_uri" = "" ]; then
         color $red "ディレクトリが存在していません。" 1>&2
         finish="true"
-    elif [ "$target_device" = "" ]; then
-        color $red "デバイスが指定されていません。指定してください。" 1>&2
-        check="true"
     fi
-    
-    # 終了フラグを回収
+fi
 
-    if [ "$finish" = "true" ]; then
-        usage_exit
-    fi
-    
+# デバイスツリーの指定をチェック
+
+if [ "$target_device" = "" ]; then
+    color $red "デバイスが指定されていません。" 1>&2
+    check="true"
+fi
+
+# 終了フラグを回収
+
+if [ "$finish" = "true" ]; then
+    usage 1>&2
+    exit 1
+fi
+
+if [ "$screen_skip" != "true" ]; then
+
     # screenで再起動
     
     screen $0 "$@" -x
@@ -152,18 +161,87 @@ if [ "$tweet" = "true" ]; then
     zip_name="*"
 fi
 
-# repo sync処理
+# repo init / ディレクトリ移動
 
-cd $source_dir
+if [ "$repo_init_uri" != "" ]; then
+
+    # ディレクトリ作成 / 移動
+
+    mkdir -p $source_dir
+    cd $source_dir
+
+    # ソース名取得
+    
+    if [ ! -e ./config.sh ]; then
+
+        color $blue "ソース名を入力してください"
+        echo -n "ソース名: "
+        read source_name
+        if [ "$source_name" = "" ]; then
+            source_name=$source_dir
+        fi
+        echo "source_name="$source_name > ./config.sh
+        color $green "ソース名 $source_name とセットされ設定が保存されました。"
+
+	else
+
+        if [ "$tweet" = "true" ]; then
+            . ./config.sh
+        fi
+
+    fi
+
+    # ツイート
+
+    if [ "$tweet" = "true" ]; then
+
+    	start_init_time=$(date '+%m/%d %H:%M:%S')
+        start_init_tweet="$source_name の repo init を行います。\n$start_init_time"
+        
+        if [ -e ../config.sh ]; then
+            . ../config.sh
+        fi
+
+        echo -e $start_init_tweet | python ../tweet.py
+  
+    fi
+
+    repo init -u $repo_init_uri
+    
+    if [ $? -ne 0 ]; then
+
+        # ツイート
+
+        if [ "$tweet" = "true" ]; then
+
+            stop_init_time=$(date '+%m/%d %H:%M:%S')
+            stop_init_tweet="$source_name の repo init が異常終了しました。\n$stop_init_time"
+
+            if [ -e ../config.sh ]; then
+                . ../config.sh
+            fi
+
+            echo -e $stop_init_tweet | python ../tweet.py
+
+        fi
+
+    fi
+
+else
+    cd $source_dir
+fi
+
+# repo sync
 
 if [ "$repo_sync" = "true" ]; then
 
     color $blue "repo syncを開始します。"
 
-    # ツイート処理
+    # ツイート
     if [ "$tweet" = "true" ]; then
 
-        # ソースフォルダ内設定ファイル読み込み
+        source build/envsetup.sh >& /dev/null
+        breakfast $target_device >& /dev/null
 
         if [ -f ./config.sh ]; then
             . ./config.sh
@@ -173,8 +251,6 @@ if [ "$repo_sync" = "true" ]; then
 
         start_sync_time=$(date '+%m/%d %H:%M:%S')
         start_sync_tweet="$source_name の repo sync を開始します。\n$start_sync_time"
-
-        # 基幹設定ファイル読み込み
 
         if [ -f ../config.sh ]; then
             . ../config.sh
@@ -188,13 +264,13 @@ if [ "$repo_sync" = "true" ]; then
 
     res_sync=$?
 
-    # ツイート処理
+    # ツイート
     if [ "$tweet" = "true" ]; then
     
         source build/envsetup.sh >& /dev/null
         breakfast $target_device >& /dev/null
 
-        # ソースフォルダ内設定ファイル読み込み
+        $res_source=$?
 
         if [ -f ./config.sh ]; then
             . ./config.sh
@@ -205,8 +281,6 @@ if [ "$repo_sync" = "true" ]; then
         end_sync_time=$(date '+%m/%d %H:%M:%S')
         stop_sync_tweet="$source_name の repo sync が異常終了しました。\n$end_sync_time"
         end_sync_tweet="$source_name の repo sync が正常終了しました。\n$end_sync_time"
-
-        # 基幹設定ファイル読み込み
 
         if [ -f ../config.sh ]; then
             . ../config.sh
@@ -219,38 +293,34 @@ if [ "$repo_sync" = "true" ]; then
         fi
 
     fi
-    
-    # 前処理
 
-    source build/envsetup.sh >& /dev/null
-    breakfast $target_device >& /dev/null
-    if [ $? -ne 0 ]; then
+    if [ $res_breakfast -ne 0 ]; then
         color $red "デバイスツリーが正常ではありません。" 1>&2
-        usage_exit
+        usage 1>&2
+        sleep 30
+        exit 1
     fi
 
 else
 
-    # 前処理
-
     source build/envsetup.sh >& /dev/null
     breakfast $target_device >& /dev/null
     if [ $? -ne 0 ]; then
         color $red "デバイスツリーが正常ではありません。" 1>&2
-        usage_exit
+        usage 1>&2
+        sleep 30
+        exit 1
     fi
 
 fi
 
-# ツイート設定処理
+# ツイート設定
 
 if [ "$tweet" = "true" ]; then
 
     # 変数初期値設定
 
     model_name=$(get_build_var PRODUCT_MODEL)
-    
-    # ソースフォルダ内設定ファイル読み込み
 
     if [ -f ./config.sh ]; then
         . ./config.sh
@@ -258,7 +328,7 @@ if [ "$tweet" = "true" ]; then
 
 fi
 
-# make clean処理
+# make clean
 
 if [ "$make_clean" = "true" ]; then
 
@@ -272,7 +342,7 @@ fi
 log_file_time=$(date '+%Y-%m-%d_%H-%M-%S')
 log_file_name="${log_file_time}_${source_dir}_${target_device}"
 
-# ツイート処理
+# ツイート
 
 if [ "$tweet" = "true" ]; then
 
@@ -280,8 +350,6 @@ if [ "$tweet" = "true" ]; then
 
     start_build_time=$(date '+%m/%d %H:%M:%S')
     start_build_tweet="$model_name 向け $source_name のビルドを開始します。\n$start_build_time"
-    
-    # 基幹設定ファイル読み込み
 
     if [ -f ../config.sh ]; then
         . ../config.sh
@@ -291,7 +359,7 @@ if [ "$tweet" = "true" ]; then
     
 fi
 
-# ビルド処理
+# ビルド
 
 LANG=C
 if [ "$make_mode" = "true" ]; then
@@ -306,16 +374,15 @@ res_build=${PIPESTATUS[0]}
 
 # ファイル移動
 if [ $res_build -eq 0 ]; then
-    mv --backup=t out/target/product/$target_device/$zip_name/ ../$zip_folder_name
+    mv --backup=t out/target/product/$target_device/$zip_name.zip ../$zip_folder_name
     mv ../$log_folder_name/$log_file_name.log ../$log_folder_name/successful/$log_file_name.log
 else
     mv ../$log_folder_name/$log_file_name.log ../$log_folder_name/failed/$log_file_name.log
 fi
 
-# 後処理
 cd ..
 
-# ツイート処理
+# ツイート
 
 if [ "$tweet" = "true" ]; then
 
@@ -332,8 +399,6 @@ if [ "$tweet" = "true" ]; then
         end_build_tweet="$model_name 向け $source_name のビルドが成功しました。\n$end_build_log\n$end_build_time"
         end_build_zip_tweet="$zip_name のビルドが成功しました。\n$end_build_log\n$end_build_time"
     fi
-
-    # 基幹設定ファイル読み込み
     
     if [ -f ./config.sh ]; then
         . ./config.sh
